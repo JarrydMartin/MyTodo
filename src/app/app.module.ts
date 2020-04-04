e227402f94d36bdb8608b36c383faaa66857109f
Jarryd Martin <emailme@jarrydmartin.dev>
2020-04-04 16:32:23 +1300

    Spelling miss take on Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import {MatInputModule} from '@angular/material/input';
 import { TodosComponent } from './Components/todos/todos.component';
 import { TodoItemComponent } from './Components/todo-item/todo-item.component';
 
-import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { AddTodoComponent } from './Components/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
